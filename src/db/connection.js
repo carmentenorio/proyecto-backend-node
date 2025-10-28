16008a84c3bb6f24b8a3e2183d01824ee427bb26
carmentenorio <carmen.tenorio.btsoft@gmail.com>
2025-10-28 09:43:05 -0400

    PBN-2: Configure SQLite database with ORM

---
+++ src/db/connection.js
@@ -0,0 +1,23 @@
+// src/db/connection.js
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: 'localhost',          
+  port: 3306, 
+  user: 'root',         
+  password: "",  
+  database:'todolist_node',      
+  waitForConnections: true,
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
+  queueLimit: 0
+});
+
+async function testConnection(){
+    const [rows] = await pool.query('SELECT 1 AS result');
+    console.log(rows);
+}
+console.log("test");
+testConnection().catch(console.error);
+
+module.exports = { pool};
